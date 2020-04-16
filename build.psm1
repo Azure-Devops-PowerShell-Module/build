ef9e1b29636eb0e10ea54a5dfe6127a70897168f
Jeff Patton <jeffrey@patton-tech.com>
2020-04-16 16:17:43 -0500

    updated readme, and added shouldprocess to new-buildfolder

--- build.psm1
+++ build.psm1
@@ -374,7 +374,7 @@ function Get-BuildFolder {
   }
 }
 function New-BuildFolder {
-  [CmdletBinding(
+  [CmdletBinding(SupportsShouldProcess, ConfirmImpact = 'Low',
     HelpURI = 'https://github.com/Azure-Devops-PowerShell-Module/build/blob/master/docs/New-AzDevOpsBuildFolder.md#new-azdevopsbuildfolder',
     PositionalBinding = $true)]
   [OutputType([Object])]
@@ -412,7 +412,9 @@ function New-BuildFolder {
         if ($Description) { Add-Member -InputObject $Body -MemberType NoteProperty -Name Description -Value $Description }
 
         $uriProjects = $Global:azDevOpsOrg + "$($Project.Id)/_apis/build/folders?path=$($Name.Replace('\','/'))&api-version=5.1-preview.2"
-        Invoke-RestMethod -Uri $uriProjects -Method Put -Headers $Global:azDevOpsHeader -Body ($Body | ConvertTo-Json) -ContentType 'application/json'
+        if ($PSCmdlet.ShouldProcess("New", "Create Folder $($Name) in $($Project.name) Azure Devops Projects")) {
+          Invoke-RestMethod -Uri $uriProjects -Method Put -Headers $Global:azDevOpsHeader -Body ($Body | ConvertTo-Json) -ContentType 'application/json'
+        }
       }
       else {
         $PSCmdlet.ThrowTerminatingError(
